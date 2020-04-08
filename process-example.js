0085135bf4e3382d7ac40fc215ea17971f4c5064
Praveen <praveen@example.com>
2020-04-08 20:13:45 +0530

    yargs

--- process-example.js
+++ process-example.js
@@ -1,12 +1,5 @@
-const a = parseFloat(process.argv[2]);
-const operator = process.argv[3];
-const b = parseFloat(process.argv[4]);
-
-if(operator === "+")
-    {
-        console.log(a + b)
-    }
-else if(operator === "-")
-    {
-        console.log(a - b)
-    }
+const argv = require('yargs').argv
+// const a = parseFloat(process.argv[2]);
+// const operator = process.argv[3];
+// const b = parseFloat(process.argv[4]);
+console.log(argv)
