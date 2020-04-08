a748f743bd47706a09d8c7cb3ac11db014622312
Praveen <praveen@example.com>
2020-04-08 20:50:08 +0530

    JSON fs

--- fs-example.js
+++ fs-example.js
@@ -26,7 +26,24 @@ const fs = require("fs");
 // const contents = fs.readFileSync("somefile.txt", "utf-8");
 // console.log("contents", contents)
 
-fs.rename("somefile.txt", "example.txt", ()=>{
-    console.log("file renamed.")
-});
+// fs.rename("somefile.txt", "example.txt", ()=>{
+//     console.log("file renamed.")
+// });
 
+// fs.readFile("person.json", "utf-8", (err, data)=>{
+//     if(err)
+//         {
+//             console.error("Error Occured.");
+//             return false;
+//         }
+//     const jsoncontent = JSON.parse(data);
+//     console.log(jsoncontent.address.flatno);
+// })
+
+const content = {
+    name : "Amit",
+    company : "Edureka",
+    hello : ()=>{}
+}
+
+console.log(content, JSON.stringify(content))
