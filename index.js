bb6aaab1d77b466f5e09d8aa4fc218cedae812c0
Praveen <praveen@example.com>
2020-04-09 20:44:47 +0530

    change for promises and callbacks.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const chalk = require('chalk');
- 
-console.log(chalk.green('Hello world!'));
 
-console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+readFile
+readFileSync
+readFile
+
+
+
+
+setTimeout(() => { console.log("0") }, 200) // Delay
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("4")
+console.log("5")
+setTimeout(() => { console.log("6") }, 1000)  // Delay
+setTimeout(() => { console.log("7") }, 0)  // Delay
+setTimeout(() => { console.log("8") }, 0) 
+console.log("9")
