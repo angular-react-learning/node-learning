215164b4327e669cc16ee832548dec22f200e464
Praveen <praveen@example.com>
2020-04-10 21:44:20 +0530

    NodeJS and MongoClient

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json({ type : "application/json" }))
+
+app.use(bodyParser.text({ type : "text/html" }))
+
+
+const MongoClient = require("mongodb").MongoClient;
+const MongoURL = "mongodb://localhost:27017";
+
+
+app.get("/movies", (req, res)=>{
+    
+    MongoClient.connect(MongoURL, (err, client)=>{
+        const db = client.db("imdb");
+        const collection = db.collection("movie");
+        collection.find({}).toArray((err, docs)=>{
+            res.send(docs);
+            client.close();
+        })
+    })
+})
+
+app.post("/movies", (req, res)=>{
+    MongoClient.connect(MongoURL, (err, client)=>{
+        const db = client.db("imdb");
+        const collection = db.collection("movie");
+        collection.insert(req.body, function(err, response){
+            res.send("inserted.")
+            client.close();
+        })
+    })
+})
+
+app.listen(6500);
