72732452ded19a8aefa31fd244baa4330d3a8f83
Praveen <praveen@example.com>
2020-04-14 19:24:56 +0530

    Assignment 1.

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const ejs = require("ejs");
+
 const mongoose = require("mongoose");
 const db = mongoose.connect("mongodb://localhost:27017/imdb", { useNewUrlParser: true })
+const user = require("./routes/user")
 
-const home = require("./routes/index")
-const name = require("./routes/name");
-const rating = require("./routes/rating");
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json({ type : "application/json" }))
 app.use(bodyParser.text({ type : "text/html" }))
+app.use(bodyParser.urlencoded({ extended : true }))
 
-app.use("/", home);
-app.use("/name", name);
-app.use("/rating", rating);
+
+app.use("/user", user);
 
 app.listen(3000, ()=>{
     console.log("Server Started...")
